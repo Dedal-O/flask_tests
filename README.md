a7f8999f61919a185a8ce8fa24b6e213c24ca2da
Taras_Logvin <t.logvinn@gmail.com>
2019-02-09 03:26:38 +0300

    version 0.1:  - have simple echo

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
-### Flask_Start
-##### Simple project for tuting Flask and testing features
-more info - later
+#### Flask_Start
+###### Simple project for tuting Flask and testing features
+
+##### version 0.1
+- simple web-server
+- receives requests and records request type and data to database 
+###### to url http://<server_name>:5000/echo/
+- has simple admin interface to view database records
+###### on url http://<server_name>:5000/admin/
+
+in plans:
+ - set deploying on prod server
+ - authorisation in admin interface 
--- main.py
+++ main.py
@@ -1,13 +1,63 @@
-from flask import Flask, request
+from os import getcwd
+from os.path import join as p_join
 from datetime import datetime
+from flask import Flask, request
+from flask_script import Manager
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+from sqlalchemy import create_engine, Column, Integer, String, DateTime
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from local_settings import DEBUG
 
 app = Flask(__name__)
+the_manager = Manager(app)
+admin = Admin(app, name='Messages Receiver', template_mode='bootstrap3')
+
+if DEBUG:
+    db_file = p_join(getcwd().replace('/src', ''), 'files', 'flask.sqlite')
+    sql_engine = create_engine(f'sqlite:///{db_file}', convert_unicode=True)
+else:
+    # connect to postgresql database
+    pass
+db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=sql_engine))
+Base = declarative_base()
+
+
+class Message(Base):
+    __tablename__ = 'messages'
+    id = Column(Integer, primary_key=True)
+    created = Column('created', DateTime, default=datetime.now())
+    method = Column('method', String)
+    body = Column('body', String)
+
+    def __repr__(self):
+        return f'{str(self.created)} - {self.method} - {self.body[:15]}'
+
+
+app.config['FLASK_ADMIN_SWITCH'] = 'cerulan'
+admin.add_view(ModelView(Message, db_session))
+
+def get_db():
+    """Start working with database"""
+    Base.query = db_session.query_property()
+    Base.metadata.create_all(bind=sql_engine)
+
+
+def close_db(error=None):
+    """To close database on Error"""
+    db_session.remove()
 
 
 @app.route('/echo/', methods=['GET', 'POST'])
 def test_receive():
     if request.method == 'POST':
-        app.logger.info(f"{datetime.now()} ECHO Received POST. Containes: {request.get_data()}")
+        app.logger.info(f"{datetime.now()} ECHO Received POST. Contains: {request.get_data()}")
+        mess = Message()
+        mess.method = 'POST'
+        mess.body = str(request.get_data().decode('utf-8'))
+        db_session.add(mess)
+        db_session.commit()
         return f"""<html><head><title></title></head>
                 <body>
                 Thanks for you data
@@ -16,7 +66,18 @@ def test_receive():
                 """
 
     if request.method == 'GET':
-        app.logger.info(f"{datetime.now()} ECHO Received GET. Containes:(will be later)'")
+        get_data = ''
+        received = {}
+        for key in request.args:
+            received[key] = request.args.get(key)
+        app.logger.info(f"{datetime.now()} ECHO Received GET. Contains: {received}")
+        print(f"{datetime.now()} ECHO Received GET. Contains: {received}")
+        mess = Message()
+#        mess.created = datetime.now()
+        mess.method = 'GET'
+        mess.body = str(received)
+        db_session.add(mess)
+        db_session.commit()
         return f"""<html><head><title></title></head>
                 <body>
                 Thanks for visit
@@ -32,5 +93,17 @@ def hello():
     """
 
 
-if __name__ == "__main__":
+@app.teardown_appcontext
+def teardown_db(error=None):
+    close_db()
+
+
+@the_manager.command
+def runserver():
     app.run()
+    get_db()
+    close_db()
+
+
+if __name__ == "__main__":
+    the_manager.run()
